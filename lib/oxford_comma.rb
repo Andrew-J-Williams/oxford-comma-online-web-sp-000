80d3abffdca4348d1198b16afafc7502f34401a4
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 21:12:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
 
-  if array.length == 2
-    return array.join(" and ")
-  elsif array.length == 3
-     test_time = ", and " + array.pop
+  if array.length == 2 #We check to see if our array has 2 elements.
+    return array.join(" and ") #If so, we 'join' the two as a string with the word 'and' between them (don't forget spaces).
+  elsif array.length == 3 #If not 2, the next 'else if' checks to see if our array has 3 elements.
+     test_time = ", and " + array.pop #
      return array.join(", ") + test_time
   elsif array.length > 3
     test_time = ", and " + array.pop
@@ -11,5 +11,5 @@ def oxford_comma(array)
   else
     return array.join
   end
-  
+
 end
