f4a50cdf8b3e0906acc2b19c97e9cbef8d375681
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 21:10:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,8 @@ def oxford_comma(array)
      test_time = ", and " + array.pop
      return array.join(", ") + test_time
   elsif array.length > 3
-    return array.join(", ")
+    test_time = ", and " + array.pop
+    return array.join(", ") + test_time
   else
     return array.join
   end
