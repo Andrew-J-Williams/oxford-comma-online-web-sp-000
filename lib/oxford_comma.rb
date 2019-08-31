b65d52526db61cc5d661c59fb361940fc3783dee
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 21:11:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
+
   if array.length == 2
     return array.join(" and ")
   elsif array.length == 3
@@ -10,4 +11,5 @@ def oxford_comma(array)
   else
     return array.join
   end
+  
 end
