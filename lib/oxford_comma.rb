00ed334a54d45f182b4774ed8322bbda165e24c1
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 21:08:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length == 3
      test_time = "and " + array.pop
-     return array.join[", "] + test_time
+     return array.join(", ") + test_time
   elsif array.length > 3
     return array.join(", ")
   else
