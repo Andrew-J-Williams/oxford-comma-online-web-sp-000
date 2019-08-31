a7992b419156364aa183ba117450d090a078a225
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 21:05:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length == 3
     last_item = array.pop
-    return array.join(", ")"and #{last_item}"
+    return array.join(", ")+"and #{last_item}"
   elsif array.length > 3
     return array.join(", ")
   else
